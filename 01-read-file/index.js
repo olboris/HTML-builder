1ca4b2f022d033d2ef60138050ba6204c6ad7d56
Olga Babushkina <boriskinaos@mail.ru>
2021-11-02 14:15:47 +0300

    fix: fix errors of 1st and 2nd tasks

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = process;
 
 const fileName = path.join(__dirname, 'text.txt');
 const readableStream = fs.createReadStream(fileName, 'utf-8');
@@ -7,14 +8,6 @@ let data = '';
 
 readableStream.on('data', chunk => data+=chunk);
 readableStream.on('end', () => {
-    let arr = data.split('');
-    let index;
-    for (let i = arr.length - 1; i>=0; i--) {
-        if ((arr[i] === '\n') & (!arr[i+1] || arr[i+1] === '\n')) {
-            index = i;
-        }
-    }
-    arr.splice(index, arr.length - index);
-    console.log(arr.join(''));
+    stdout.write(data);
 });  
 readableStream.on('error', error => console.log('Error', error.message));
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,26 +1,19 @@
 const fs = require('fs');
 const readline = require('readline');
 const { stdout } = process;
+const output = fs.createWriteStream('./02-write-file/text.txt');
 
 let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
  });
 
-function writeFile(text) {
-  fs.appendFile('./02-write-file/text.txt', text+'\n', function(err) {
-    if(err) {
-        return (err);
-    }
-  });
-}
-
 stdout.write('Введите текст (для выхода нажмите ctrl+c или введите exit)\n');
 rl.on('line', (line) => {
   if (line === 'exit') {
     rl.close();
   } else {
-    writeFile(line);
+    output.write(line);
   }
 });
 rl.on('close', () => {
