d712114058e51d015ea9ba66f8036f275226945f
Olga Babushkina <boriskinaos@mail.ru>
2021-11-04 12:38:23 +0300

    feat: add fifth task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,5 @@
 const { copyFile } = require('fs');
+const { stdout } = process;
 const path = require('path');
 const { mkdir } = require('fs/promises');
 const fs = require('fs');
@@ -7,18 +8,18 @@ function copyDir() {
     mkdir(path.dirname(__filename)+'/files-copy', { recursive: true });
     fs.readdir(path.dirname(__filename)+'/files', (err, files) => {
         if (err)
-          console.log(err);
+        stdout.write(err);
         else {
           files.forEach(file => {  
               copyFile(`${path.dirname(__filename)}/files/${file}`, `${path.dirname(__filename)}/files-copy/${file}`, function(err) {
                 if (err) {
-                  console.log("Error Found:", err);
+                  stdout.write("Error Found:", err);
                 }
               });
+            });
+          }
         });
-}
-    });
-    console.log('Файлы директории "files" скопированы в директорию "files-copy"');
+      stdout.write('Файлы директории "files" скопированы в директорию "files-copy"');
   }
 
 copyDir();
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+let data = '';
+let folderPath = path.dirname(__filename)+'/styles/';
+
+fs.readdir(folderPath, (err, files) => {
+    if (err)
+    stdout.write(err);
+    else {
+        files.forEach(file => {  
+            fs.stat(folderPath+file, (err, stats) => {
+              if (err) {
+                stdout.write(err)
+                return
+              }
+              if (stats.isFile() && path.parse(folderPath+file).ext === '.css') {
+                const output = fs.createWriteStream(path.dirname(__filename)+'/project-dist/'+'bundle.css');
+                const readableStream = fs.createReadStream(folderPath+file, 'utf-8');
+                readableStream.on('data', chunk => data+=chunk);
+                readableStream.on('end', () => {
+                    output.write(data);
+                });
+              };
+            });
+        });
+    }
+    stdout.write('Стили записаны в файл bundle.css в папке project-dist');
+});
