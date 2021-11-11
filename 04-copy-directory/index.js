6059c5f0a6bd9fe195ca9b03f43f4391263d4e8f
Olga Babushkina <boriskinaos@mail.ru>
2021-11-11 09:35:38 +0300

    fix: fourth task solution - clean copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,14 @@ const fs = require('fs');
 
 function copyDir() {
     mkdir(path.dirname(__filename)+'/files-copy', { recursive: true });
+    fs.readdir(path.dirname(__filename)+'/files-copy', (err, files) => {
+      if (err) throw err;
+      for (const file of files) {
+        fs.unlink(path.join(path.dirname(__filename)+'/files-copy', file), err => {
+          if (err) throw err;
+        });
+      }
+    });
     fs.readdir(path.dirname(__filename)+'/files', (err, files) => {
         if (err)
         stdout.write(err);
